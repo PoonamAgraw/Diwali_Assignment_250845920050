de8f7e91b4ee32b1a393197b6535f96b33552d88
Poonam Agrawal <poonamsagrawal31@gmail.com>
2025-10-20 12:51:29 +0530

    Day 2

---
+++ 20-10-2025/SumOfOdd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumOfEven(int n) {
+    int sum = 0;
+    for (int i = 1; i <= 2 * n; i += 2) {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int sum = sumOfEven(n);
+    cout << "Sum of first " << n << " odd natural numbers is " << sum;
+    return 0;
+}
